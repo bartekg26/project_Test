c088d8e52674eaa0cf6dab59b4e1fafff483a112
bartekg26 <bxaxrx12@wp.pl>
2016-03-19 17:51:01 +0100

    Revert "Revert "plik""
    This reverts commit 9affa5f9bbc0bc15c90256344bc16d453e4290ce.

--- BD2 MapReduce/Program.cs
+++ BD2 MapReduce/Program.cs
@@ -20,6 +20,7 @@ namespace BD2_MapReduce
 
             List<KeyValuePair<int, int>> wynik_reducera = reducer.Zsumuj(wynik);
 
+            Console.WriteLine("ok");
             //System.Console.WriteLine(wynik_reducera);
             System.Console.ReadLine();
         }
