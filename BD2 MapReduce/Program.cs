7a53e201edbec099d767e956b9d89d722a00e408
bartekg26 <bxaxrx12@wp.pl>
2016-03-19 19:00:45 +0100

    mh j
    jmmmj

--- BD2 MapReduce/Program.cs
+++ BD2 MapReduce/Program.cs
@@ -21,7 +21,7 @@ namespace BD2_MapReduce
             List<KeyValuePair<int, int>> wynik_reducera = reducer.Zsumuj(wynik);
             
            
-            Console.WriteLine("okeyzmienionooooooooooooooooo");
+            Console.WriteLine("okeyzmienionoooooooooooooooooooouu");
 
             //System.Console.WriteLine(wynik_reducera);
             System.Console.ReadLine();
