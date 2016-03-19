72453c110748c84aaa1ae2f8498aabc17804fb73
bartekg26 <bxaxrx12@wp.pl>
2016-03-19 17:59:41 +0100

    Komit
    numer 2

--- BD2 MapReduce/Program.cs
+++ BD2 MapReduce/Program.cs
@@ -20,7 +20,7 @@ namespace BD2_MapReduce
 
             List<KeyValuePair<int, int>> wynik_reducera = reducer.Zsumuj(wynik);
 
-            Console.WriteLine("ok");
+            Console.WriteLine("okey");
             //System.Console.WriteLine(wynik_reducera);
             System.Console.ReadLine();
         }
